90add884f773b98ac40cad902585a3dc9c276e9a
Gregorio <Greg.del801@gmail.com>
2023-06-29 08:35:31 -0700

    completed lab

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// function expression example
+const divide = function(x, y) {
+    return x/y
+}
+
+// arrow function example
+const square = number => number * number
+
+const add = (num1, num2) => num1 + num2
